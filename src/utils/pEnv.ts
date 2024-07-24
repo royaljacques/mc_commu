d660298815424d7b47e0c3c81a2b06c55ab947ed
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2024-07-24 02:08:08 +0200

    Add files via upload

--- src/utils/pEnv.ts
+++ src/utils/pEnv.ts
@@ -13,7 +13,7 @@ const DATABASE_USERNAME = process.env.DATABASE_USERNAME;
 const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 const DATABASE_NAME = process.env.DATABASE_NAME;
 // Check for missing environment variables
-if (!BOT_TOKEN || !BOT_ID || !BOT_SECRET || !GUILD_OWNER_ID || !DATABASE_HOST || !DATABASE_USERNAME || !DATABASE_PASSWORD || !DATABASE_NAME) {
+if (!BOT_TOKEN || !BOT_ID || !BOT_SECRET || !GUILD_OWNER_ID) {
     throw new Error("Missing environment variables");
 }
 
